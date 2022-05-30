035cb22fd416dfce8d32addf6ac3ef27851eef60
turgutersozmobitek <98022847+turgutersozmobitek@users.noreply.github.com>
2022-05-30 16:17:34 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,22 +1,3 @@
-terraform {
-  required_providers {
-    vcd = {
-      source = "vmware/vcd"
-      version = "3.6.0"
-    }
-  }
-}
-
-# Connect VMware vCloud Director Provider
-provider "vcd" {
-  user                 = var.vcd_user
-  password             = var.vcd_pass
-  org                  = "System"
-  url                  = var.vcd_url
-  max_retry_timeout    = var.vcd_max_retry_timeout
-  allow_unverified_ssl = var.vcd_allow_unverified_ssl
-}
-
 # Create new External Network
 
 resource "vcd_external_network" "extnet" {
