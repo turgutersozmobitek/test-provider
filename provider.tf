93541351457706ee140971d885c8567e72086cc9
turgutersozmobitek <98022847+turgutersozmobitek@users.noreply.github.com>
2022-05-30 16:29:09 +0300

    Delete provider.tf

--- provider.tf
+++
@@ -1,9 +0,0 @@
-# Connect VMware vCloud Director Provider
-provider "vcd" {
-  user                 = var.vcd_user
-  password             = var.vcd_pass
-  org                  = "System"
-  url                  = var.vcd_url
-  max_retry_timeout    = var.vcd_max_retry_timeout
-  allow_unverified_ssl = var.vcd_allow_unverified_ssl
-}
